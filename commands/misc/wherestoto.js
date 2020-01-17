3e5fffa5101d3f2de867ee4452f6cfa9cd6c5ea9
clickyfreewheel <clicky.freewheel@pm.me>
2020-01-17 10:40:25 -0800

    Added wherestoto command

---
+++ commands/misc/wherestoto.js
@@ -0,0 +1,36 @@
+const debug = require('debug')('bot:hi')
+const moment = require('moment')
+const commando = require('discord.js-commando')
+const oneLine = require('common-tags').oneLine
+
+module.exports = class Time extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'wherestoto',
+      aliases: [],
+      group: 'misc',
+      memberName: 'wherestoto',
+      description: 'Where is Toto?',
+      details: oneLine`
+      `,
+      examples: ['wherestoto']
+    })
+  }
+
+  async run(msg, args) {
+    const weekday = moment.day()
+    const hour = moment.hour()
+
+    // Weekend
+    if (weekday === 0 || weekday === 6) {
+      if (hour >= 0 && hour <= 10) return msg.reply('Toto is sleeping!')
+      if (hour >= 11) return msg.reply('Toto is gaming!')
+    }
+
+    // Weekday
+    if (hour >= 0 && hour <= 8) return msg.reply('Toto is sleeping!')
+    if (hour >= 9 && hour <= 16) return msg.reply('Toto is working!')
+    if (hour === 17) return msg.reply('Toto is driving!')
+    if (hour >= 18 && hour <= 23) return msg.reply('Toto is gaming!')
+  }
+}
